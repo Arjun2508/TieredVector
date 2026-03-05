1c27d66a813fb64902244217671410d7bb235cf9
Arjun2508 <arjun250806@gmail.com>
2026-03-06 02:16:54 +0530

    Fixed Critical bug in push_back operation
    (sz >> 10) >= block_sz is the new condition

--- TieredVector.cpp
+++ TieredVector.cpp
@@ -192,7 +192,7 @@ class tiered_vector{
         }
 
         void push_back(const T& value){
-            if((sz&1023) == 0){
+            if((sz >> 10) >= block_sz){
                 initNextSubArray();
             }
             pdata[sz>>10][sz&1023] = value;
@@ -200,7 +200,7 @@ class tiered_vector{
         }
 
         void push_back(T&& value){
-            if((sz&1023) == 0){
+            if((sz >> 10) >= block_sz){
                 initNextSubArray();
             }
             pdata[sz>>10][sz&1023] = move(value);
